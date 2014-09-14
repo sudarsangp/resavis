6de673f55fcd674a30dfe0b3d3e67bf6a998ccd1
sudarsangp <sudarsangp@gmail.com>
2014-09-14 23:30:52 +0800

    implemented constants and basic exception handling. Refactored current code in main py file

--- readRedisQueue.py
+++ readRedisQueue.py
@@ -4,11 +4,50 @@ import ast
 from flask import Flask, render_template
 import logging
 import datetime
+import sys
+from utilities import const
+
+""" constants used in the program that can be assigned only once """
+const.KEY_DURATION = "duration"
+const.KEY_SEND_QUEUE = "send-queue"
+const.KEY_RECV_QUEUE = "recv-queue"
+const.KEY_EXECUTE = "execute"
+
+const.KEY_COMPONENT_BASIC = "ComponentBasic"
+const.KEY_COMPONENT_DETAIL = "ComponentDetail"
+const.KEY_SEND_QUEUE_DETAIL = "SendQueue"
+const.KEY_RECV_QUEUE_DETAIL = "ReceiveQueue"
+
+const.KEY_READ_POS = "read_pos"
+const.KEY_WRITE_POS = "write_pos"
+const.KEY_CAPACITY = "capacity"
+const.KEY_POPULATION = "population"
+
+const.KEY_STREAM = "stream"
+const.KEY_VALUE = "value"
+
+const.COLON = ':'
+const.HIPHEN_GREATHERTHAN = '->'
+const.COMMA = ','
+
+const.DEFAULT_LOG_DIRECTORY = "log/"
+const.DEFAULT_LOG_FILE_NAME = "timediff.log"
+const.DEFAULT_LOGGER_LEVEL = logging.DEBUG
+const.DEFAULT_WRITE_FILENAME = "datafromredis"
+const.DEFAULT_FILE_MODE = 'w'
+
+const.HOST = "localhost"
+const.START_INDEX_FOR_REDIS = 0
+const.END_INDEX_FOR_REDIS = -1
+const.REDIS_QUEUE_NAME = "2fwc-1-1410146732-metrics"
+
+const.HTML_INDEX = "index.html"
+const.HTML_ERROR = "error.html"
 
 class ComponentBasicInfo:
-	component_string = "Component"
-	task_string = "Task"
-	time_stamp_string = "Timestamp"
+	COMPONENT = "Component"
+	TASK = "Task"
+	TIMESTAMP = "Timestamp"
 	def __init__(self, component, task, time_stamp):
 		self.component = component
 		self.task = task
@@ -16,13 +55,13 @@ class ComponentBasicInfo:
 
 	@staticmethod
 	def parse_component_basic(parse_data):
-		return parse_data.split(':')
+		return parse_data.split(const.COLON)
 
 class ComponentDetailInfo:
-	duration_string = "Duration"
-	send_queue_string = "Send Queue"
-	recv_queue_string = "Recv Queue"
-	execute_string = "Execute"
+	DURATION = "Duration"
+	SEND_QUEUE = "Send Queue"
+	RECV_QUEUE = "Recv Queue"
+	EXECUTE = "Execute"
 	def __init__(self, duration, send_queue, recv_queue, execute):
 		self.duration = duration
 		self.send_queue = send_queue
@@ -33,21 +72,21 @@ class ComponentDetailInfo:
 	def parse_component_detail(list_parse_data):
 		parse_component_detail = {}
 		for key in list_parse_data:
-			if str(key) == "duration":
-				parse_component_detail["duration"] = list_parse_data[key]
-			elif str(key) == "send-queue":
-				parse_component_detail["send-queue"] = list_parse_data[key]
-			elif str(key) == "recv-queue":
-				parse_component_detail["recv-queue"] = list_parse_data[key]
-			elif str(key) == "execute":
-				parse_component_detail["execute"] = list_parse_data[key]
+			if str(key) == const.KEY_DURATION:
+				parse_component_detail[const.KEY_DURATION] = list_parse_data[key]
+			elif str(key) == const.KEY_SEND_QUEUE:
+				parse_component_detail[const.KEY_SEND_QUEUE] = list_parse_data[key]
+			elif str(key) == const.KEY_RECV_QUEUE:
+				parse_component_detail[const.KEY_RECV_QUEUE] = list_parse_data[key]
+			elif str(key) == const.KEY_EXECUTE:
+				parse_component_detail[const.KEY_EXECUTE] = list_parse_data[key]
 		return parse_component_detail
 
 class SendQueueDetail:
-	read_pos_string = "Read Position"
-	write_pos_string = "Write Position"
-	capacity_string = "Capacity"
-	population_string = "Population"
+	READ_POSITION = "Read Position"
+	WRITE_POSITION = "Write Position"
+	CAPACITY = "Capacity"
+	POPULATION = "Population"
 	def __init__(self, read_pos, write_pos, capacity, population):
 		self.read_pos = read_pos
 		self.write_pos = write_pos
@@ -58,21 +97,21 @@ class SendQueueDetail:
 	def parse_send_queue(send_queue):
 		parse_send_queue = {}
 		for key in send_queue:
-			if str(key) == "read_pos":
-				parse_send_queue["read_pos"] = send_queue[key]
-			elif str(key) == "write_pos":
-				parse_send_queue["write_pos"] = send_queue[key]
-			elif str(key) == "capacity":
-				parse_send_queue["capacity"] = send_queue[key]
-			elif str(key) == "population":
-				parse_send_queue["population"] = send_queue[key]
+			if str(key) == const.KEY_READ_POS:
+				parse_send_queue[const.KEY_READ_POS] = send_queue[key]
+			elif str(key) == const.KEY_WRITE_POS:
+				parse_send_queue[const.KEY_WRITE_POS] = send_queue[key]
+			elif str(key) == const.KEY_CAPACITY:
+				parse_send_queue[const.KEY_CAPACITY] = send_queue[key]
+			elif str(key) == const.KEY_POPULATION:
+				parse_send_queue[const.KEY_POPULATION] = send_queue[key]
 		return parse_send_queue
 
 class RecvQueueDetail:
-	read_pos_string = "Read Position"
-	write_pos_string = "Write Position"
-	capacity_string = "Capacity"
-	population_string = "Population"
+	READ_POSITION = "Read Position"
+	WRITE_POSITION = "Write Position"
+	CAPACITY = "Capacity"
+	POPULATION = "Population"
 	def __init__(self, read_pos, write_pos, capacity, population):
 		self.read_pos = read_pos
 		self.write_pos = write_pos
@@ -83,21 +122,21 @@ class RecvQueueDetail:
 	def parse_recv_queue(recv_queue):
 		parse_recv_queue = {}
 		for key in recv_queue:
-			if str(key) == "read_pos":
-				parse_recv_queue["read_pos"] = recv_queue[key]
-			elif str(key) == "write_pos":
-				parse_recv_queue["write_pos"] = recv_queue[key]
-			elif str(key) == "capacity":
-				parse_recv_queue["capacity"] = recv_queue[key]
-			elif str(key) == "population":
-				parse_recv_queue["population"] = recv_queue[key]
+			if str(key) == const.KEY_READ_POS:
+				parse_recv_queue[const.KEY_READ_POS] = recv_queue[key]
+			elif str(key) == const.KEY_WRITE_POS:
+				parse_recv_queue[const.KEY_WRITE_POS] = recv_queue[key]
+			elif str(key) == const.KEY_CAPACITY:
+				parse_recv_queue[const.KEY_CAPACITY] = recv_queue[key]
+			elif str(key) == const.KEY_POPULATION:
+				parse_recv_queue[const.KEY_POPULATION] = recv_queue[key]
 		return parse_recv_queue
 
 class ExecuteDetail:
-	stream_string = "Stream"
-	count_string = "Count"
-	sum_string =  "Sum"
-	sum_squares_string = "Sum of Squares"
+	STREAM = "Stream"
+	COUNT = "Count"
+	SUM =  "Sum"
+	SUM_OF_SQUARES = "Sum of Squares"
 	def __init__(self, stream, count, sum_value, sum_squares):
 		self.stream = stream
 		self.count = count
@@ -110,26 +149,29 @@ class ExecuteDetail:
 			return None
 		parse_execute = {}
 		for key in execute:
-			parse_execute["stream"] = key
-			parse_execute["value"] = execute[key]
+			parse_execute[const.KEY_STREAM] = key
+			parse_execute[const.KEY_VALUE] = execute[key]
 		return parse_execute
 
-def initialize_logger(filename="timediff.log", logger_level = logging.DEBUG):
-	logging.basicConfig(filename = 'log/' + filename, level = logger_level)
+def initialize_logger(filename=const.DEFAULT_LOG_FILE_NAME, logger_level = const.DEFAULT_LOGGER_LEVEL):
+	logging.basicConfig(filename = const.DEFAULT_LOG_DIRECTORY + filename, level = logger_level)
 
 def connect_to_redis(host, start_index, stop_index, redis_queue_name):
-	start = datetime.datetime.now()
-	r_server = redis.Redis(host)
-	result_from_server = r_server.lrange(redis_queue_name, start_index , stop_index)
-	end = datetime.datetime.now()
-	time_diff = end - start
-	data_size = len(result_from_server)
-	logging.debug('Time to connect to redis on %s and read data of size %s is %s' %(host, str(data_size), str(time_diff)))
-	return result_from_server
+	try:
+		start = datetime.datetime.now()
+		r_server = redis.Redis(host)
+		result_from_server = r_server.lrange(redis_queue_name, start_index , stop_index)
+		end = datetime.datetime.now()
+		time_diff = end - start
+		data_size = len(result_from_server)
+		logging.debug('Time to connect to redis on %s and read data of size %s is %s' %(host, str(data_size), str(time_diff)))
+		return result_from_server
+	except:
+		print "Redis Server not online",sys.exc_info()[0]
 
 """ write data to file datafromredis which is read from redis server"""
-def write_redis_data_to_file():
-	file_write = open('datafromredis', 'w')
+def write_redis_data_to_file(filename = const.DEFAULT_WRITE_FILENAME, file_mode = const.DEFAULT_FILE_MODE):
+	file_write = open(filename, file_mode)
 	file_write.write(str(result_from_server))
 
 def store_redis_data_in_objects(result_from_server):
@@ -138,34 +180,34 @@ def store_redis_data_in_objects(result_from_server):
 	tuple_details = {}
 	for i in range(len(result_from_server)):
 
-		parse_data = result_from_server[i].split('->')
+		parse_data = result_from_server[i].split(const.HIPHEN_GREATHERTHAN)
 		measure_data = ComponentBasicInfo.parse_component_basic(parse_data[0])
 		component_basic = ComponentBasicInfo(measure_data[0], measure_data[1], measure_data[2])
-		tuple_details["ComponentBasic"] = component_basic
+		tuple_details[const.KEY_COMPONENT_BASIC] = component_basic
 		list_parse_data = ast.literal_eval(parse_data[1])
 
 		parse_component_detail = ComponentDetailInfo.parse_component_detail(list_parse_data)
-		if "execute" in parse_component_detail:
-			component_detail = ComponentDetailInfo(parse_component_detail["duration"], parse_component_detail["send-queue"], parse_component_detail["recv-queue"], parse_component_detail["execute"])
+		if const.KEY_EXECUTE in parse_component_detail:
+			component_detail = ComponentDetailInfo(parse_component_detail[const.KEY_DURATION], parse_component_detail[const.KEY_SEND_QUEUE], parse_component_detail[const.KEY_RECV_QUEUE], parse_component_detail["execute"])
 		else:
-			component_detail = ComponentDetailInfo(parse_component_detail["duration"], parse_component_detail["send-queue"], parse_component_detail["recv-queue"], None)
+			component_detail = ComponentDetailInfo(parse_component_detail[const.KEY_DURATION], parse_component_detail[const.KEY_SEND_QUEUE], parse_component_detail[const.KEY_RECV_QUEUE], None)
 
-		tuple_details["ComponentDetail"] = component_detail
+		tuple_details[const.KEY_COMPONENT_DETAIL] = component_detail
 		parse_send_queue = SendQueueDetail.parse_send_queue(component_detail.send_queue)
-		send_queue = SendQueueDetail(parse_send_queue["read_pos"], parse_send_queue["write_pos"], parse_send_queue["capacity"], parse_send_queue["population"])
-		tuple_details["SendQueue"] = send_queue
+		send_queue = SendQueueDetail(parse_send_queue[const.KEY_READ_POS], parse_send_queue[const.KEY_WRITE_POS], parse_send_queue[const.KEY_CAPACITY], parse_send_queue[const.KEY_POPULATION])
+		tuple_details[const.KEY_SEND_QUEUE_DETAIL] = send_queue
 
 		parse_recv_queue = RecvQueueDetail.parse_recv_queue(component_detail.recv_queue)
-		recv_queue = RecvQueueDetail(parse_recv_queue["read_pos"], parse_recv_queue["write_pos"], parse_recv_queue["capacity"], parse_recv_queue["population"])
-		tuple_details["ReceiveQueue"] = recv_queue
+		recv_queue = RecvQueueDetail(parse_recv_queue[const.KEY_READ_POS], parse_recv_queue[const.KEY_WRITE_POS], parse_recv_queue[const.KEY_CAPACITY], parse_recv_queue[const.KEY_POPULATION])
+		tuple_details[const.KEY_RECV_QUEUE_DETAIL] = recv_queue
 
 		parse_execute = ExecuteDetail.parse_execute(component_detail.execute)
 		if parse_execute is not None:
-			execute_values = parse_execute["value"].split(',')
-			execute = ExecuteDetail(parse_execute["stream"], execute_values[0], execute_values[1], execute_values[2])
-			tuple_details["Execute"] = execute
+			execute_values = parse_execute[const.KEY_VALUE].split(const.COMMA)
+			execute = ExecuteDetail(parse_execute[const.KEY_STREAM], execute_values[0], execute_values[1], execute_values[2])
+			tuple_details[ComponentDetailInfo.EXECUTE] = execute
 		else:
-			tuple_details["Execute"] = None
+			tuple_details[ComponentDetailInfo.EXECUTE] = None
 
 		tuple_objects.append(tuple_details)
 		tuple_details = {}
@@ -187,10 +229,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	initialize_logger()
-	result = connect_to_redis("localhost", 0, -1 , "2fwc-1-1410146732-metrics")
+	result = connect_to_redis(const.HOST, const.START_INDEX_FOR_REDIS, const.END_INDEX_FOR_REDIS , const.REDIS_QUEUE_NAME)
+	if result is None:
+		return render_template(const.HTML_ERROR)
+
 	data = store_redis_data_in_objects(result)
-	print data[0]["ComponentBasic"].__dict__
-	return render_template("index.html", data = data)
+	print data[0][const.KEY_COMPONENT_BASIC].__dict__
+	return render_template(const.HTML_INDEX, data = data)
 
 if __name__ == "__main__":
 	app.run(debug = True)
